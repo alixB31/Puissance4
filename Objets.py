dc7934e8bc809cc0d071810f95c67de5448fa347
alixB31 <131276536+alixB31@users.noreply.github.com>
2024-02-06 11:53:33 +0100

    début jeux

--- Objets.py
+++ Objets.py
@@ -7,25 +7,82 @@ class Joueur:
 class Grille:
     # Initalisation
     def __init__(self):
-        self.hauteur = 6
-        self.largeur = 7
+        self.nbLignes = 6
+        self.nbColonnes = 7
         self.grille = []
 
+    # Getter
+    def getNbColonnes(self):
+        return self.nbColonnes
+
+    # Getter
+    def getNbLignes(self):
+        return self.nbLignes
+    
+    # Getter
+    def getGrille(self):
+        return self.grille
+    
+    def insertion(self):
+        nLigne = 0
+        nColonne = 0
+        for _ in range(self.nbLignes):
+            nLigne = nLigne + 1 
+            
+            ligne = []
+            for _ in range(self.nbColonnes):
+                nColonne = nColonne + 1
+                # Création et ajout d'un objet Case à la ligne
+                ligne.append(Case(nLigne,nColonne)) 
+            # Ajout de la ligne à la grille
+            self.grille.append(ligne) 
+            nColonne = 0
+
+    # Accéder à une case spécifique dans la grille
+    def getCaseSpecifique(self, ligne, colonne):
+        if 0 <= ligne < self.nbLignes and 0 <= colonne < self.nbColonnes:
+            return self.grille[ligne][colonne]
+        else:
+            return None
+        
+    # Affichage de la grille
+    def afficher_grille(self):
+        grille = [[' ' for _ in range(self.nbColonnes)] for _ in range(self.nbLignes)]
+        # Crée le bord supérieur de la grille
+        print("\n+" + "--" * 13 + "-+")
+        for ligne in grille:
+            # Crée le début de chaque ligne avec une barre verticale
+            print("|", end="")
+            for case in ligne:
+                print("   |", end='')
+            print("\n+" + "---+" * 7)
+        # Affiche les indices des colonnes en bas de la grille
+        print ("  1"+ "   2"+ "   3"+"   4"+"   5"+"   6"+"   7")
+
+    
 class Case:
     # Initalisation
-    def __init__(self):
+    def __init__(self,ligne,colonne):
+        self.ligne = ligne
+        self.colonne = colonne
         self.estVide = True
         self.couleurCase = ""
 
     # Getter
-    @property
+    def getColonne(self):
+        return self.colonne
+    
+    # Getter
+    def getLigne(self):
+        return self.ligne
+    
+    # Getter
     def getEstVide(self):
-        return self.__estVide
+        return self.estVide
 
     # Getter
-    @property
     def getCouleurCase(self):
-        return self.__couleurCase
+        return self.couleurCase
 
 class Pion:
     # Initalisation
--- jeux.py
+++ jeux.py
@@ -1,5 +1,26 @@
-import Objets
+import Objets  # Importe le module Objets
+import random
 
 # Permet de lancer la partie
 if __name__ == "__main__":
+
+    # Création de l'objet grille
     grille = Objets.Grille()
+
+    # Demande le nom du joueur
+    nomUtilisateur = input("Veuillez saisir votre nom : ")
+    nombreAleatoire = random.choice(["Croix", "Rond"])
+    # Création du joueur et association d'une couleur
+    joueur = Objets.Joueur(nomUtilisateur,nombreAleatoire)
+
+    # Insertions des objets cases dans la grille
+    grille.insertion()
+
+    # Affichage de la grille
+    affichageGrille = grille.afficher_grille()
+
+    choix = input("Veuillez Choisir la ou vous voulez jouer : ")
+    if not choix.isdigit():
+        print("L'entrée n'est pas un entier.")
+    else:
+        print("Commence")
